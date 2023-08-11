293f88e414ef536d092bb1db91430e283c7546dd
Masood-B <masoodbasardien123@gmail.com>
2023-08-11 16:14:19 +0200

    connection

--- api/config/index.js
+++ api/config/index.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const { createPool } = require('mysql');
 // Create connection variable
-let connection = createPool({
+const connection = createPool({
     host: process.env.dbHost,
     user: process.env.dbUser,
     password: process.env.dbPwd,
-    port: process.env.dbPort,
+    // port: process.env.dbPort,
     database: process.env.dbName,
     multipleStatements: true,
     connectionLimit: 30
 });
-module.exports = connection;
+model.exports = {
+    connection
+}
